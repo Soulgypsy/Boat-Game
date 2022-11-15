f728f1365f34d645e16348cdbc52c4c3b832afb6
Gabriel-Harvey <gharve201@caledonian.ac.uk>
2022-11-15 02:22:02 +0000

    Harpoon Level Polish

--- Assets/Scripts/HarpoonBoatMovement.cs
+++ Assets/Scripts/HarpoonBoatMovement.cs
@@ -7,13 +7,21 @@ public class HarpoonBoatMovement : MonoBehaviour
     [SerializeField] private GameObject harpoon;
     [SerializeField] private float speed;
     public bool moving;
+    public float rotateSpeed = 5f;
     
     void Update()
     {
         if (moving == true)
         {
             transform.position = Vector3.MoveTowards(transform.position, harpoon.transform.position, speed * Time.deltaTime); //*
+
+            var targetRotation = Quaternion.LookRotation(harpoon.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
         }
+
+        
+
+        
     }
 
     public void Actived(GameObject poon)
